533382f4a66eae853a12b182f5a8d829d3be8615
Hatakee01 <125679277+Hatakee01@users.noreply.github.com>
2024-11-11 21:31:12 -0600

    Add files via upload

---
+++ app/Http/Controllers/Api/AuthuserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+use \stdClass;
+
+
+class AuthuserController extends Controller
+{
+    // Registro de usuario
+    public function register(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()], 400); // Cambié a 400 para errores de validación
+    }
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = $user->createToken('authToken')->plainTextToken;
+
+    return response()->json([
+        'data' => $user,
+        'access_token' => 'Bearer ' . $token, // Asegúrate de incluir el token aquí
+    ], 201); // Cambié a 201 para indicar que se creó un recurso
+}
+
+    // Inicio de sesión
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) 
+        {
+            return response()
+            ->json(['message' => 'No autorizado'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()
+        ->json([
+            'message' => 'Hi, ' . $user->name,
+            'access_token' => $token,
+             'token_type' => 'Bearer',
+             'user' => $user,
+            ], 200);
+    }
+
+    // Cierre de sesión
+
+    public function logout()
+    {
+        auth()->user()->Tokens()->delete();
+
+        return[
+            'message' => 'Has cerrado sesión exitosamente y el token se eliminó exitosamente'
+        ];
+    }
+
+    // Listar usuarios
+
+    public function index()
+    {
+        return User::all();
+    }
+
+    // Mostrar usuario por id
+    public function show($id)
+    {
+        return User::find($id);
+    }
+}
